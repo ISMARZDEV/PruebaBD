5f52fb829fa34acb4f240fe6b189c7457e71afcd
Ismael Martínez <1077546@est.intec.edu.do>
2023-12-31 12:55:31 -0400

    BD Insertar datos

--- Program.cs
+++ Program.cs
@@ -14,17 +14,53 @@ class Program
             connection.Open();
             Console.WriteLine("Conexión exitosa");
 
+            string Salir = "N", tipodocumento, documento, nombres, apellidos, sexo, fechanacimiento, nota;
+
+            while (Salir == "N")
+            {
+            Console.Write("Tipo Documento: ");
+            tipodocumento = Console.ReadLine();
+            Console.Write("Documento: ");
+            documento = Console.ReadLine();
+            Console.Write("Nombres: ");
+            nombres = Console.ReadLine();
+            Console.Write("Apellidos: ");
+            apellidos = Console.ReadLine();
+            Console.Write("Sexo: ");
+            sexo = Console.ReadLine();
+            Console.Write("Fecha Nacimiento: ");
+            fechanacimiento = Console.ReadLine();
+            Console.Write("Nota: ");
+            nota = Console.ReadLine();
+            
             // Insertar datos en la tabla
             using (SqlCommand insertDataCommand = new SqlCommand(
-                "INSERT INTO Empleados (IdEmpleado, Nombre, Apellido, Edad, Cargo) VALUES " +
-                "(1, 'Ismael', 'Martinez', 24, 'Jefe' )", connection))
+                $"INSERT INTO TuTablaBD (tipodocumento, documento, nombres, apellidos, sexo, fechanacimiento, nota) VALUES " +
+                $"({tipodocumento}, {documento}, '{nombres}', '{apellidos}', '{sexo}', '{fechanacimiento}', '{nota}')", connection))
             {
-            insertDataCommand.ExecuteNonQuery();
-            Console.WriteLine("Datos insertados exitosamente");
+                insertDataCommand.ExecuteNonQuery();
+                Console.WriteLine("Datos insertados exitosamente");
+                Console.WriteLine("Y/N");
+                Salir = Console.ReadLine();
+            }
             }
         }
     }
 }
+
+/*
+CREATE TABLE [dbo].[TuTablaBD] (
+    [id]              INT           IDENTITY (1, 1) NOT NULL,
+    [tipodocumento]   INT           NULL,
+    [documento]       INT           NULL,
+    [nombres]         VARCHAR (100) NULL,
+    [apellidos]       VARCHAR (100) NULL,
+    [sexo]            VARCHAR (10)  NULL,
+    [fechanacimiento] DATE          NULL,
+    [nota]            VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+*/
 
 /* try
             {
